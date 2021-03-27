3dde2e2dba954d0aa02532c4862c756863a95368
Maxime <maxime@fevrier.io>
2021-03-27 15:17:19 +0100

    wip(Details)

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,5 +1,53 @@
 import React from 'react';
+import {useQuery} from 'react-query';
+import {Player} from '../../types';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../../../App';
 
-export default function Details(): JSX.Element {
-  return (<></>);
+type GetPlayerProps = {
+  queryKey: [string, {playerId: number; season: number}];
+};
+
+async function getPlayer(params: GetPlayerProps) {
+  const [, {playerId, season}] = params.queryKey;
+  const response = await fetch(
+    `https://api.monpetitgazon.com/stats/player/${playerId}?season=${season}`,
+  );
+  if (!response.ok) {
+    throw new Error('Problem fetching data');
+  }
+  return await response.json();
+}
+
+type DetailsScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Details'
+>;
+
+type Params = {
+  playerId: string;
+};
+
+type Route = {
+  params: Params;
+};
+
+type DetailsProps = {
+  route: Route;
+  navigation: DetailsScreenNavigationProp;
+};
+
+export default function Details({
+  route,
+  navigation,
+}: DetailsProps): JSX.Element {
+  const {playerId} = route.params;
+  const {status, error, data} = useQuery<Player, Error>(
+    ['player', {playerId: playerId, season: 2018}],
+    getPlayer,
+  );
+
+  console.log(')-)-)))-', data);
+
+  return <></>;
 }
