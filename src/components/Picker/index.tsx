ddabb660f9283765d4fe3b81627d87207759d0e0
Maxime <maxime@fevrier.io>
2021-03-28 11:23:54 +0200

    feat(Picker): add close picker

--- src/components/Picker/index.tsx
+++ src/components/Picker/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {useCallback, useEffect, useState} from 'react';
 import {Picker} from '@react-native-community/picker';
 import {ItemValue} from '@react-native-community/picker/typings/Picker';
+import styled from 'styled-components/native';
 
 export type PickerItems = {
   label: string;
@@ -10,12 +11,14 @@ export type PickerItems = {
 type PickerItemProps = {
   onValueChange: (value: PickerItems) => void;
   pickerItems: PickerItems[];
+  closePicker: () => void;
   defaultValue?: PickerItems;
 };
 
 export default function PickerItem({
   onValueChange,
   pickerItems = [],
+  closePicker,
   defaultValue,
 }: PickerItemProps): JSX.Element {
   const [currentValue, setCurrentValue] = useState<ItemValue>(
@@ -38,16 +41,37 @@ export default function PickerItem({
   );
 
   return (
-    <Picker
-      mode={'dialog'}
-      selectedValue={currentValue}
-      enabled={true}
-      onValueChange={onDataChange}>
-      {pickerItems.map((item, index) => {
-        return (
-          <Picker.Item key={index} label={item.label} value={item.value} />
-        );
-      })}
-    </Picker>
+    <React.Fragment>
+      <CloseContainer onPress={closePicker}>
+        <CustomText>{'Fermer'}</CustomText>
+      </CloseContainer>
+      <Picker
+        mode={'dialog'}
+        selectedValue={currentValue}
+        enabled={true}
+        onValueChange={onDataChange}>
+        {pickerItems.map((item, index) => {
+          return (
+            <Picker.Item key={index} label={item.label} value={item.value} />
+          );
+        })}
+      </Picker>
+    </React.Fragment>
   );
 }
+
+const CloseContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 12px;
+  border-top-width: 1px;
+  border-top-color: lightgrey;
+  border-style: solid;
+`;
+
+const CustomText = styled.Text`
+  font-size: 14px;
+  font-weight: 500;
+  color: #0e7afe;
+`;
