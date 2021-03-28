2a693bc617a04a2c1e63e628b0c6ca36d0d3e282
Maxime <maxime@fevrier.io>
2021-03-28 19:18:29 +0200

    feat: error

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,28 @@
+import React, {useMemo} from 'react';
+import styled from 'styled-components/native';
+import {Text, TextStyle} from 'react-native';
+
+type ErrorProps = {
+  message: string;
+};
+
+export default function Error({message}: ErrorProps): JSX.Element {
+  const textStyle: TextStyle = useMemo(() => {
+    return {color: 'red', fontSize: 24, fontWeight: '500', textAlign: 'center'};
+  }, []);
+
+  return (
+    <ErrorContainer>
+      <Text style={textStyle}>
+        {message ??
+          'Une erreur est survenue. \n Merci de bien vouloir réessayer ultérieurement'}
+      </Text>
+    </ErrorContainer>
+  );
+}
+
+const ErrorContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
