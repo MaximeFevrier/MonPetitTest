bb898838773fd307140b6d178fcfd96733e0f506
Maxime <maxime@fevrier.io>
2021-03-29 09:02:43 +0200

    feat(useSearchFilter): add multiple conditions

--- src/hooks/useSearchFilter.ts
+++ src/hooks/useSearchFilter.ts
@@ -6,16 +6,25 @@ export default function useSearchFilter(
 ): [Player[], string, (text: string) => void] {
   const [searchValue, setSearchValue] = useState<string>('');
   const [searchData, setSearchData] = useState<Player[]>([]);
+  const [isSearched, setIsSearched] = useState<boolean>(false);
 
   let onSearchDataChange = useCallback((newSearchData: Player[]) => {
     setSearchData(newSearchData);
   }, []);
 
   useEffect(() => {
-    if (!searchData?.length && data?.length) {
+    if (searchValue?.length > 0) {
+      setIsSearched(true);
+    } else {
+      setIsSearched(false);
+    }
+  }, [searchValue]);
+
+  useEffect(() => {
+    if (!searchData?.length && data?.length && !isSearched) {
       onSearchDataChange(data);
     }
-  }, [data, onSearchDataChange, searchData]);
+  }, [data, isSearched, onSearchDataChange, searchData]);
 
   let onChangeText = useCallback(
     (text: string) => {
